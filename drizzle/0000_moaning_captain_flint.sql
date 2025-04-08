b95735b06a21712d1bfa7e8e14bbd0b46acb9b8f
Gustavo Marques <gugamello2014@yahoo.com>
2025-04-08 09:46:59 -0300

    feat: update database and schema

--- drizzle/0000_moaning_captain_flint.sql
+++ drizzle/0000_moaning_captain_flint.sql
@@ -1,15 +1,15 @@
-CREATE TABLE IF NOT EXISTS `guests` (
+CREATE TABLE `guests` (
 	`id` text PRIMARY KEY NOT NULL,
 	`name` text NOT NULL,
-	`email` text NOT NULL
+	`phone` text NOT NULL
 );
 --> statement-breakpoint
-CREATE UNIQUE INDEX IF NOT EXISTS `guests_email_unique` ON `guests` (`email`);--> statement-breakpoint
-CREATE TABLE IF NOT EXISTS `stays` (
+CREATE UNIQUE INDEX `guests_phone_unique` ON `guests` (`phone`);--> statement-breakpoint
+CREATE TABLE `stays` (
 	`id` text PRIMARY KEY NOT NULL,
 	`guests` integer NOT NULL,
 	`guest_id` text NOT NULL,
-	`password` text DEFAULT '' NOT NULL,
+	`password` text NOT NULL,
 	`check_in` text NOT NULL,
 	`check_out` text NOT NULL,
 	FOREIGN KEY (`guest_id`) REFERENCES `guests`(`id`) ON UPDATE no action ON DELETE no action
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -3,7 +3,7 @@ import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 export const guests = sqliteTable('guests', {
   id: text('id').primaryKey().notNull(),
   name: text('name').notNull(),
-  email: text('email').notNull().unique(),
+  phone: text('phone').notNull().unique(),
 });
 
 export const stays = sqliteTable('stays', {
@@ -12,7 +12,7 @@ export const stays = sqliteTable('stays', {
   guest_id: text('guest_id')
     .notNull()
     .references(() => guests.id),
-  password: text('password').notNull().default(''),
+  password: text('password').notNull(),
   check_in: text('check_in').notNull(),
   check_out: text('check_out').notNull(),
 });
