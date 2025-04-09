7c144b9ccc5d4a30a1abd5759bfc0abec5dccb74
Gustavo Marques <gugamello2014@yahoo.com>
2025-04-09 20:57:58 -0300

    feat: implement static params generation for stays

--- src/app/[stay_id]/page.tsx
+++ src/app/[stay_id]/page.tsx
@@ -1,5 +1,7 @@
+import { db } from '@/db/firebase';
 import { getStay } from '@/modules/stay/services/stay.service';
 import { StayView } from '@/modules/stay/view/stay.view';
+import { collection, getDocs } from 'firebase/firestore';
 
 type Props = {
   params: Promise<{
@@ -12,4 +14,16 @@ export default async function StayPage({ params }: Props) {
   const stay = await getStay(stay_id);
 
   return <StayView stay={stay} />;
+}
+
+export const revalidate = 600;
+export async function generateStaticParams() {
+  const querySnapshot = await getDocs(collection(db, 'stays'));
+  const ids = querySnapshot.docs.map((doc) => {
+    return doc.id;
+  });
+
+  return ids.map((id) => ({
+    stay_id: id,
+  }));
 }
