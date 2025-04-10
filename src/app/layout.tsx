95c91f6372feede922941cfa023d3088aeb2461f
Gustavo Marques <gugamello2014@yahoo.com>
2025-04-09 21:01:15 -0300

    feat: enable dark mode in RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
-    <html lang='pt-BR' className={roboto.variable}>
+    <html lang='pt-BR' className={`${roboto.variable} dark`}>
       <body className='font-main antialiased'>{children}</body>
     </html>
   );
