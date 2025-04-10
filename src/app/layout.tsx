a589335cca1d9e284a29387d6486899af71625a3
Gustavo Marques <gugamello2014@yahoo.com>
2025-04-09 21:41:38 -0300

    feat: remove Clarity dependency and integrate script directly in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,7 @@
-import Clarity from '@microsoft/clarity';
 import type { Metadata } from 'next';
 import { Roboto } from 'next/font/google';
 import { PropsWithChildren } from 'react';
 import './globals.css';
-
-const projectId = 'r1pgueco5e';
-
-Clarity.init(projectId);
 
 const roboto = Roboto({
   variable: '--font-roboto',
@@ -23,6 +18,17 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang='pt-BR' className={`${roboto.variable} dark`}>
+      <head
+        dangerouslySetInnerHTML={{
+          __html: `<script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "r1pgueco5e");
+</script>`,
+        }}
+      />
       <body className='font-main antialiased'>{children}</body>
     </html>
   );
