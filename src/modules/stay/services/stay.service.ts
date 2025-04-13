909fdeb140acb1fce31eaec5cd4f9d5ace928259
Gustavo Marques <gugamello2014@yahoo.com>
2025-04-13 19:36:40 -0300

    feat: implement StayDi for dependency injection in getStay service

--- src/modules/stay/services/stay.service.ts
+++ src/modules/stay/services/stay.service.ts
@@ -1,11 +1,12 @@
 'use server';
 
-import { Fetcher } from '@/lib/fetcher';
+import { StayDi } from '@/api/infra/di/stay/stay_di';
 import { GetStayOutput, getStayOutputSchema } from './stay.dto';
 
 export async function getStay(stay_id: string): Promise<GetStayOutput> {
-  const response = await Fetcher.fetch(`/api/stay/${stay_id}`);
-  const parsedStay = getStayOutputSchema.parse(response);
-
-  return parsedStay;
+  const di = new StayDi();
+  const useCase = di.makeGetStayUseCase();
+  const output = await useCase.execute({ stay_id });
+  const parsedOutput = getStayOutputSchema.parse(output);
+  return parsedOutput;
 }
